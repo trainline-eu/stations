855d0108a695e78200da44513e79a803a5bcb826
Edouard de Labareyre <edouard@capitainetrain.com>
2017-06-14 11:09:41 +0200

    Add two boolean columns, one to tag airport related stations, another one to tag cities with ambiguous names accross Europe
    All set to false for the moment

--- test_data.rb
+++ test_data.rb
@@ -65,7 +65,7 @@ class StationsTest < Minitest::Test
   end
 
   def test_number_columns
-    nb_columns = 35 + (Constants::CARRIERS.size * 2)
+    nb_columns = 37 + (Constants::CARRIERS.size * 2)
 
     STATIONS.each { |row| assert_equal nb_columns, row.size, "Station #{row["name"]} (#{row["id"]}) has a wrong number of columns: #{row["size"]}" }
   end
