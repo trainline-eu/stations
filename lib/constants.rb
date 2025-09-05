a5a24426eea75b5c77109e4a64ddfc042d10b367
Luca Vezzoli <luca.vezzoli@thetrainline.com>
2025-09-05 11:34:14 +0200

    Fix Malpensa Aeroporto station mapping
    This PR fixes the Malpensa Aeroporto station mapping for Distribusion.
    
    The specific codes for Terminal 1 and Terminal 2 are not yielding results and are not used.
    The Terminal 2 specific station code (used by all our connected coach carriers) has been fixed to the one that is actually returning results, while the Terminal 1 specific code has been removed because:
    
    * it is the defaullt destination selected when using the area code, as it is closest to Milan
    * the different enabled coach carriers enabled in Distribusion are using several, different station codes arount Terminal 1, so using the area is better and more maintenable.

--- lib/constants.rb
+++ lib/constants.rb
@@ -429,7 +429,6 @@ module Constants
     "74660", # Taranto Terminal Cimino
     "74661", # Firenze Piazzale Montelungo
     "8673", # Utrecht Centraal - Distribusion - European Sleeper (train)
-    "20530", # Malpensa Airport T1
     "28958", # Malpensa Airport T2
   ]
 end
